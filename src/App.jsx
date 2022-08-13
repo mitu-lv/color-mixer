c0ea65956bd1750affc49d5e9950f110237a2114
Mikus Lacarus <mikus.lacarus@prezi.com>
2022-08-13 13:09:46 +0300

    Small small css styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {mixLayersOfColors} from './colors';
 export function App() {
     const [layerOfColors, setLayerOfColors] = useState(['rgba(0,0,0,0.1)', '#ffffff'])
     const [result, setResult] = useState('none');
+
     const handleSubmit= (e) => {
         e.preventDefault();
         const result = mixLayersOfColors([...layerOfColors]);
@@ -19,12 +20,23 @@ export function App() {
         setLayerOfColors([...layerOfColors]);
     }
 
+    function addItem(e) {
+        layerOfColors.unshift('rgba(0,0,0,0.1)');
+        setLayerOfColors([...layerOfColors]);
+    }
+
+    function updateItem(e) {
+        const index = Number(e.target.dataset.index);
+        layerOfColors[index] = e.target.value;
+        setLayerOfColors([...layerOfColors]);
+    }
+
     return (
         <div>
-            <h1>Mix colors by putting transparent colors one on top of the other</h1>
-            <div>
+            <div class="align-transform-colors">
+                <h1>Mix colors by putting transparent colors one on top of the other</h1>
                 <form id="transform-colors" onSubmit={e => handleSubmit(e)}>
-                    <button type="button" className="add-button" onClick={e => { layerOfColors.unshift('rgba(0,0,0,0.1)');setLayerOfColors([...layerOfColors]);}}>+</button>
+                    <button type="button" className="add-button" onClick={addItem}>+</button>
                     {layerOfColors.map((color, index) => (
                         <label key={index} className="row color-entry">
                             <div className="color" style={{backgroundColor: color}}></div>
@@ -32,16 +44,17 @@ export function App() {
                                 type="text"
                                 placeholder="eg. #000000, rgb(0,0,0), rgba(0,0,0,0.1)"
                                 value={color}
-                                onChange={e => {layerOfColors[index] = e.target.value; setLayerOfColors([...layerOfColors]);}}
+                                data-index={index}
+                                onChange={updateItem}
                             />
                             <button type="button" className="remove-button" data-index={index} onClick={removeItem}>-</button>
                         </label>
                     ))}
                     <button type="submit" className="submit">Create color</button>
                 </form>
-            </div>
-            <div className="row">
-                <div className="color" style={{backgroundColor: result}}></div><div>{result}</div>
+                <div className="row">
+                    <div className="color" style={{backgroundColor: result}}></div><div>{result}</div>
+                </div>
             </div>
         </div>
     );
